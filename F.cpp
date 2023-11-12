d6804515707132472e22994b7f3c18a8765946bd
Serafim <serafim@MacBook-Pro-Serafim.local>
2023-11-12 17:29:35 +0300

    commit

--- F.cpp
+++ F.cpp
@@ -1,77 +1,90 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
-const long long kMagicNumber = 10000000 + 4321;
-const long long kMagicNumber123 = 123;
-const long long kMagicNumber45 = 45;
+class MinHeap {
+ public:
+  MinHeap() { size_ = 0; }
 
-void FillArr(std::vector<long long>& arr, int number) {
-  for (int i = 2; i < number; ++i) {
-    arr[i] = (arr[i - 1] * kMagicNumber123 + arr[i - 2] * kMagicNumber45) %
-        (kMagicNumber);
+  static int Parent(int index) { return (index - 1) / 2; }
+  static int LeftChild(int index) { return 2 * index + 1; }
+  static int RightChild(int index) { return 2 * index + 2; }
+
+  void PushUp(int index) {
+    if (index == 0 || heap_[Parent(index)] <= heap_[index]) {
+      return;
+    }
+    Swap(index, Parent(index));
+    PushUp(Parent(index));
   }
-}
 
-int GivePivot(int left, int right) {
-  int random_index = (left + right) / 2;
-  return random_index;
-}
-
-std::pair<int, int> Partition(std::vector<long long>& arr, int left,
-                              int right) {
-  int pivot_ind = GivePivot(left, right);
-  long long pivot = arr[pivot_ind];
-  int equal_left = pivot_ind;
-  int equal_right = pivot_ind;
-  for (int i = equal_left - 1; i >= left; --i) {
-    if (arr[i] == pivot) {
-      std::swap(arr[i], arr[equal_left - 1]);
-      equal_left--;
-    } else if (arr[i] > pivot) {
-      std::swap(arr[i], arr[equal_left - 1]);
-      std::swap(arr[equal_left - 1], arr[equal_right]);
-      equal_left--;
-      equal_right--;
+  void PushDown(int index) {
+    if (LeftChild(index) > size_ - 1) {
+      return;
+    }
+    int min_index = LeftChild(index) == size_ - 1 ||
+        heap_[LeftChild(index)] < heap_[RightChild(index)]
+                    ? LeftChild(index)
+                    : RightChild(index);
+    if (heap_[min_index] < heap_[index]) {
+      Swap(index, min_index);
+      PushDown(min_index);
     }
   }
-  for (int i = equal_right + 1; i <= right; ++i) {
-    if (arr[i] == pivot) {
-      std::swap(arr[equal_right + 1], arr[i]);
-      equal_right++;
-    } else if (arr[i] < pivot) {
-      std::swap(arr[i], arr[equal_right + 1]);
-      std::swap(arr[equal_right + 1], arr[equal_left]);
-      equal_right++;
-      equal_left++;
-    }
+
+  void Insert(long long number) {
+    heap_.push_back(number);
+    size_++;
+    PushUp(size_ - 1);
   }
-  return std::pair<int, int>(equal_left, equal_right);
-}
+
+  long long GetMin() { return heap_[0]; }
+
+  void ExtractMin() {
+    Swap(0, size_ - 1);
+    size_--;
+    heap_.pop_back();
+    PushDown(0);
+  }
+
+  void DecreaseKey(int index, long long delta) {
+    heap_[index] -= delta;
+    PushUp(index);
+  }
+
+ private:
+  std::vector<long long> heap_;
+  int size_;
+
+  void Swap(int index1, int index2) { std::swap(heap_[index1], heap_[index2]); }
+};
 
 int main() {
-  int number;
-  int k_stat;
-  long frst;
-  long scond;
-  std::cin >> number >> k_stat >> frst >> scond;
-  k_stat--;
-  std::vector<long long> arr(number);
-  arr[0] = frst;
-  arr[1] = scond;
-  FillArr(arr, number);
-  int left = 0;
-  int right = number - 1;
-  int can_use = 1;
-  while (can_use == 1) {
-    std::pair<int, int> mid = Partition(arr, left, right);
-    if (mid.first <= k_stat && k_stat <= mid.second) {
-      can_use = 0;
-      std::cout << arr[k_stat];
-    } else if (mid.first > k_stat) {
-      right = mid.first - 1;
-    } else {
-      left = mid.second + 1;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  int count;
+  int index;
+  long long delta;
+  long long number;
+  std::string type;
+  std::cin >> count;
+  MinHeap min_heap;
+
+  for (int i = 0; i < count; ++i) {
+    std::cin >> type;
+    if (type == "insert") {
+      std::cin >> number;
+      min_heap.Insert(number);
+    } else if (type == "getMin") {
+      std::cout << min_heap.GetMin() << "\n";
+    } else if (type == "extractMin") {
+      min_heap.ExtractMin();
+    } else if (type == "decreaseKey") {
+      std::cin >> index >> delta;
+      index--;
+      min_heap.DecreaseKey(index, delta);
     }
   }
+
   return 0;
 }
