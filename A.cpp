51a2eead7716ae9599d214eef49cb1284d2822f2
Serafim <serafim@MacBook-Pro-Serafim.local>
2023-12-16 19:57:26 +0300

    добавил задачушечки

--- A.cpp
+++ A.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 #include <vector>
 
-struct Point {
-  int64_t x_cord;
-  int64_t y_cord;
-  Point() : x_cord(0), y_cord(0) {}
-  Point(int64_t x_cord, int64_t y_cord) : x_cord(x_cord), y_cord(y_cord) {}
-};
-
 struct Node {
   int64_t val;
   Node* next = nullptr;
-  int64_t count;
-  Node(int64_t val) : val(val), next(nullptr), count(1) {}
+  Node(int64_t val) : val(val), next(nullptr) {}
 };
 
 class HashTable {
@@ -23,8 +15,7 @@ class HashTable {
   }
 
   void Insert(int64_t val) {
-    if (Find(val) != nullptr) {
-      Find(val)->count += 1;
+    if (Find(val)) {
       return;
     }
     int64_t index = HashFunc(val);
@@ -46,29 +37,19 @@ class HashTable {
     this_node->next = new_node;
   };
 
-  Node* Find(int64_t val) {
+  bool Find(int64_t val) {
     int64_t index = HashFunc(val);
     Node* find_node = table_[index];
     while (find_node != nullptr) {
       if (find_node->val == val) {
-        return find_node;
+        return true;
       }
       find_node = find_node->next;
     }
-    return nullptr;
+    return false;
   };
 
-  int64_t Count(int64_t val) {
-    if (Find(val) == nullptr) {
-      return 0;
-    }
-    return Find(val)->count;
-  }
-
   void Erase(int64_t val) {
-    if (Find(val) == nullptr) {
-      return;
-    }
     int64_t index = HashFunc(val);
     Node* cur = table_[index];
     Node* prev = nullptr;
@@ -86,17 +67,6 @@ class HashTable {
       prev = cur;
       cur = cur->next;
     }
-  }
-
-  void Clear() {
-    for (Node* head : table_) {
-      while (head != nullptr) {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-      }
-    }
-    table_.resize(size_, nullptr);
   }
 
   ~HashTable() {
@@ -117,36 +87,27 @@ class HashTable {
   const int64_t kP = 1000000007;
 
   int64_t HashFunc(const int64_t kVal) const {
-    return ((kA * (kVal % kP) + kB) % kP) % size_;
+    return ((kA * kVal + kB) % kP) % size_;
   }
 };
 
-int64_t Distance(Point& p1, Point& p2) {
-  return (p1.x_cord - p2.x_cord) * (p1.x_cord - p2.x_cord) +
-         (p1.y_cord - p2.y_cord) * (p1.y_cord - p2.y_cord);
-}
-
 int main() {
-  int64_t number;
-  int64_t type;
-  std::cin >> number >> type;
-  std::vector<Point> points(number, Point());
-  for (int64_t i = 0; i < number; ++i) {
-    std::cin >> points[i].x_cord >> points[i].y_cord;
-  }
-  HashTable table(number);
-  int64_t ans = 0;
-  for (int64_t i = 0; i < number; ++i) {
-    for (int64_t j = 0; j < number; ++j) {
-      table.Insert(Distance(points[i], points[j]));
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::string type;
+  int number;
+  int64_t input;
+  std::cin >> number;
+  HashTable tab(number);
+  for (int i = 0; i < number; ++i) {
+    std::cin >> type >> input;
+    if (type == "+") {
+      tab.Insert(input);
+    } else if (type == "-") {
+      tab.Erase(input);
+    } else {
+      std::cout << (tab.Find(input) ? "YES" : "NO") << "\n";
     }
-    for (int64_t j = 0; j < number; ++j) {
-      int64_t count = table.Count(Distance(points[i], points[j]));
-      table.Erase(Distance(points[i], points[j]));
-      ans += (count) * (count - 1) / 2;
-    }
-    table.Clear();
   }
-  std::cout << (type == 1 ? ans : 0);
   return 0;
 }
